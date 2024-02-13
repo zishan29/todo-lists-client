83da2d5eb827afdea1729a6ecec55e47f3fd5a49
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-02-13 21:08:33 +0530

    Add firebase configuration and update README

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+![Preview image](./public/todo-preview.png)
+
+[Live Preview](https://todo-lists-d13cb.web.app)
+
 ## Getting Started
 
 First, run the development server:
---
+++ app/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQZJepRVZAKdZb8RwEMzRtsksGLZDs6S4",
+  authDomain: "todo-lists-d13cb.firebaseapp.com",
+  projectId: "todo-lists-d13cb",
+  storageBucket: "todo-lists-d13cb.appspot.com",
+  messagingSenderId: "494526315872",
+  appId: "1:494526315872:web:234dfdf28d14897270f205",
+};
+
+const app = initializeApp(firebaseConfig);
