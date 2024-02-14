ac56059911115917136f81b42a435f2e8fbae273
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-02-14 21:28:18 +0530

    Add loader to task list for initial load

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,10 @@ import { setTasks } from "./reducers/taskReducer";
 import { useDispatch, useSelector } from "react-redux";
 import { setLoading } from "./reducers/loadingReducer";
 
+interface LoadingState {
+  loading: boolean;
+}
+
 interface Task {
   name?: string;
   description?: string;
@@ -21,6 +25,9 @@ interface TaskState {
 export default function Home() {
   const dispatch = useDispatch();
   const tasks = useSelector((state: { tasks: TaskState }) => state.tasks.tasks);
+  const loading = useSelector(
+    (state: { loading: LoadingState }) => state.loading.loading
+  );
 
   useEffect(() => {
     updateTasks();
@@ -45,7 +52,11 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center pt-24 bg-neutral-800 gap-6">
       <div className="text-white font-semibold text-3xl">My Todos</div>
       <TaskInput updateTasks={updateTasks} />
-      {tasks.length > 0 ? (
+      {loading ? (
+        <div className="flex-col gap-4 w-max flex">
+          <div className="w-8 h-8 border-4 text-orange-400 text-4xl animate-spin border-white flex items-center border-t-orange-400 rounded-full"></div>
+        </div>
+      ) : tasks.length > 0 ? (
         <TaskList tasks={tasks} updateTasks={updateTasks} />
       ) : (
         <div className="text-white text-lg">No todos to display</div>
